ac6de06641e7cc16b066be823e89d5b54a8f6f42
Mateus Araujo <empresarial.mateus25@gmail.com>
2026-02-26 19:02:30 -0300

    estilização

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,17 +2,48 @@ import { getData } from './utils/getData.js';
 import { verificacao } from './utils/verificacao.js';
 import { homeGsap } from './utils/animations/homeGsap.js';
 
-    document.addEventListener('DOMContentLoaded', () => {
-        homeGsap();
-    });
-function init () {
+document.addEventListener('DOMContentLoaded', () => {
+    homeGsap();
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 120 },
+    color: { value: "#e2e3e2" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#28c941",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab"
+      }
+    }
+  },
+  retina_detect: true
+});
+
+function init() {
 
     const btn = document.querySelector('button');
-    
+
     btn.addEventListener("click", (e) => {
         const nome = document.getElementById('nome');
         const div = document.querySelector('.resultado');
-    
+
         e.preventDefault();
         const valor = nome.value;
         if (!verificacao(valor)) {
